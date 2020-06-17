9d5a743259c1842b69835bbadc29b35c2f844e43
Rebecca Rose <rebecca.rose@oliversrealfood.com.au>
2020-06-17 12:50:03 +1000

    Add horizontal scroll and nested content in, changed to now showing movies

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,7 +1,7 @@
 const api_key = '5623f69e33ca66e95505afa97a7d797d'
 
-export function getMovies() {
-    const endpoint = window.encodeURI('https://api.themoviedb.org/3/discover/movie?api_key=' + api_key)
+export function getMoviesShowing() {
+    const endpoint = window.encodeURI('https://api.themoviedb.org/3/movie/now_playing?api_key=' + api_key)
 
     return fetch(endpoint)
             .then((response) =>  response.json())
