d7108f6b19963d2d4cec7e4e1aac5508657582e3
New Author Name <rebecca.j.rose@hotmail.com>
2020-06-17 12:04:47 +1000

    Display specified reults data rather then the whole returned JSON string

--- app/components/Movies.js
+++ app/components/Movies.js
@@ -1,29 +1,53 @@
 import React from 'react';
 import { getMovies } from '../utils/api'
 
+function CardView( { repos }) {
+    console.log(repos.results)
+    return (
+
+        <ul>
+        {repos.results.map((results, index) =>  {
+            const { title, release_date } = results
+            return (
+            <li key={index}>
+                <h4>{title}</h4>
+                <h5>{release_date}</h5>
+            </li>
+            )
+        })}
+        </ul>
+    )
+}
+
 export default class Movies extends React.Component {
     constructor(props) {
         super(props);
-        this.state ={
-            repos: {}
+        this.state = {
+            repos: null
         }
     }
 
     componentDidMount() {
        getMovies()
            .then((repos) => this.setState({
-               repos: {
-                   ...repos
-               }
+                repos
            }))
         console.log(this.state.repos)
+    }
+
+    isLoading() {
+        const { repos } = this.state
+
+        return !repos
     }
 
     render(){
         const { repos } = this.state
         return (
                 <React.Fragment>
-                    {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+                    {this.isLoading() && <p>LOADING</p>}
+
+                    {repos && <CardView repos={repos}/>}
                 </React.Fragment>
 
         )
