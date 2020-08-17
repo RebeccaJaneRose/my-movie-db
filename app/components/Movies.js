cfacadd0922501aa15af145da3665d28916bbab2
Rebecca Rose <rebecca.j.rose@hotmail.com>
2020-08-18 09:31:42 +1000

    commit before merge

--- app/components/Movies.js
+++ app/components/Movies.js
@@ -1,31 +1,47 @@
 import React from 'react';
 import { getMovies } from '../utils/api'
 
+function ContentGrid ({ repos }) {
+    return(
+         <ul>
+            {repos.map((repos, index) => {
+                const {original_title} = repos
+
+                return (
+                    <li>
+                        <h1>{original_title}</h1>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
 export default class Movies extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state ={
-            repos: {}
-        }
+constructor(props) {
+    super(props);
+    this.state ={
+        repos: {}
     }
+}
 
-    componentDidMount() {
-       getMovies()
-           .then((repos) => this.setState({
-               repos: {
-                   ...repos
-               }
-           }))
-        console.log(this.state.repos)
-    }
+componentDidMount() {
+   getMovies()
+       .then((repos) => this.setState({
+           repos: {
+               ...repos
+           }
+       }))
+    console.log(this.state.repos)
+}
 
-    render(){
-        const { repos } = this.state
-        return (
-                <React.Fragment>
-                    {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
-                </React.Fragment>
+render(){
+    const { repos } = this.state
+    return (
+            <React.Fragment>
+                {repos && <ContentGrid repos={repos}/>}
+            </React.Fragment>
 
-        )
-    }
+    )
+}
 }
